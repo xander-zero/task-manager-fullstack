bf8fddef5f070a0d19901e6fa32daa69d3aea84e
xander-zero <mahmud.ansari60636@gmail.com>
2023-05-21 18:44:36 +0430

    feat: add some function to utils project

---
+++ src/utils/hooks/auth.js
@@ -0,0 +1,23 @@
+import { compare, hash } from "bcryptjs";
+import { verify } from "jsonwebtoken";
+
+async function hashPassword(password) {
+  const hashedPassword = await hash(password, 12);
+  return hashedPassword;
+}
+
+async function verifyPassword(password, hashedPassword) {
+  const isValid = await compare(password, hashedPassword);
+  return isValid;
+}
+
+function verifyToken(token, secretKey) {
+  try {
+    const result = verify(token, secretKey);
+    return { email: result.email };
+  } catch (error) {
+    return false;
+  }
+}
+
+export { hashPassword, verifyPassword, verifyToken };
